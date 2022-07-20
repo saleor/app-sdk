6540dc8001e850ec18443d957109087d5b57dd5c
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-07-20 16:19:49 +0200

    Apply code formatting

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ SDK for building great Saleor Apps.
 
 <div>
 
-  [![npm version badge](https://img.shields.io/npm/v/@saleor/app-sdk)](https://www.npmjs.com/package/@saleor/app-sdk)
-  [![npm downloads count](https://img.shields.io/npm/dt/@saleor/app-sdk)](https://www.npmjs.com/package/@saleor/app-sdk)
+[![npm version badge](https://img.shields.io/npm/v/@saleor/app-sdk)](https://www.npmjs.com/package/@saleor/app-sdk)
+[![npm downloads count](https://img.shields.io/npm/dt/@saleor/app-sdk)](https://www.npmjs.com/package/@saleor/app-sdk)
 
 </div>
 
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,38 @@
-import path from "path";
 import fg from "fast-glob";
-
 import { print } from "graphql/language/printer.js";
+import path from "path";
 
-const capitalize = (value: string) =>
-  value.charAt(0).toUpperCase() + value.slice(1);
+const capitalize = (value: string) => value.charAt(0).toUpperCase() + value.slice(1);
 
 const dropFileExtension = (filename: string) => path.parse(filename).name;
 
-export const inferWebhooks = async (baseURL: string, webhooksPath: string, generatedGraphQL: any) => {
+export const inferWebhooks = async (
+  baseURL: string,
+  webhooksPath: string,
+  generatedGraphQL: any
+) => {
   let entries;
+
   if (process.env.NODE_ENV === "production") {
     entries = await fg(["*.js"], { cwd: webhooksPath });
   } else {
-    entries = await fg(["*.ts"], { cwd: `pages/api/webhooks` });
+    entries = await fg(["*.ts"], { cwd: "pages/api/webhooks" });
   }
 
   return entries.map(dropFileExtension).map((name: string) => {
-    const camelcaseName = name.split("-").map(capitalize).join("");
+    const camelCaseName = name.split("-").map(capitalize).join("");
 
-    const eventName = name.toUpperCase().replace(new RegExp("-", "g"), "_");
+    const eventName = name.toUpperCase().replace(/-/g, "_");
     let eventType: string;
-    if (
-      Object.values(generatedGraphQL.WebhookEventTypeAsyncEnum).includes(
-        eventName
-      )
-    ) {
+    if (Object.values(generatedGraphQL.WebhookEventTypeAsyncEnum).includes(eventName)) {
       eventType = "asyncEvents";
-    } else if (
-      Object.values(generatedGraphQL.WebhookEventTypeSyncEnum).includes(
-        eventName
-      )
-    ) {
+    } else if (Object.values(generatedGraphQL.WebhookEventTypeSyncEnum).includes(eventName)) {
       eventType = "syncEvents";
     } else {
       throw Error("Event type not found.");
     }
 
-    const statement = `${camelcaseName}SubscriptionDocument`;
+    const statement = `${camelCaseName}SubscriptionDocument`;
     let query: string;
     if (statement in generatedGraphQL) {
       query = print((generatedGraphQL as any)[statement]);
