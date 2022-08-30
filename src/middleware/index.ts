ff0a03c2152403b95f44d383cf7745cc6fa9dfed
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-08-30 09:17:11 +0200

    Fix export from new modules

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,6 @@
 export * from "./with-auth-token-required";
 export * from "./with-base-url";
+export * from "./with-jwt-verified";
 export * from "./with-registered-saleor-domain-header";
 export * from "./with-saleor-domain-present";
 export * from "./with-saleor-event-match";
