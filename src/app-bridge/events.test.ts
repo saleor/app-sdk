bac42c0e1f8216eb67192567616f126f7d766b7c
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-09-06 16:57:02 +0200

    Add events factory

---
+++ src/app-bridge/events.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it } from "vitest";
+
+import { DashboardEventFactory } from "./events";
+
+describe("DashboardEventFactory", () => {
+  it("Creates handshake event", () => {
+    expect(DashboardEventFactory.createHandshakeEvent("mock-token")).toEqual({
+      payload: {
+        token: "mock-token",
+        version: 1,
+      },
+      type: "handshake",
+    });
+  });
+  it("Creates redirect event", () => {
+    expect(DashboardEventFactory.createRedirectEvent("/new-path")).toEqual({
+      payload: {
+        path: "/new-path",
+      },
+      type: "redirect",
+    });
+  });
+  it("Creates dispatch response event", () => {
+    expect(DashboardEventFactory.createDispatchResponseEvent("123", true)).toEqual({
+      payload: {
+        actionId: "123",
+        ok: true,
+      },
+      type: "response",
+    });
+  });
+  it("Creates theme change event", () => {
+    expect(DashboardEventFactory.createThemeChangeEvent("light")).toEqual({
+      payload: {
+        theme: "light",
+      },
+      type: "theme",
+    });
+  });
+});
