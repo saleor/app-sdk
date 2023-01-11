a5fc9824687a2a8205cb5d3b211fc10a10340045
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-01-11 14:09:32 +0100

    Remove console log

--- src/APL/auth-data-from-object.ts
+++ src/APL/auth-data-from-object.ts
@@ -1,12 +1,15 @@
 import { AuthData } from "./apl";
+import { createAPLDebug } from "./apl-debug";
 import { hasAuthData } from "./has-auth-data";
+
+const debug = createAPLDebug("authDataFromObject");
 
 /**
  * Returns AuthData if the object follows it's structure
  */
 export const authDataFromObject = (parsed: unknown): AuthData | undefined => {
   if (!hasAuthData(parsed)) {
-    console.debug("Given object did not contained AuthData");
+    debug("Given object did not contained AuthData");
     return undefined;
   }
   const { apiUrl, appId, domain, token, jwks } = parsed as AuthData;
