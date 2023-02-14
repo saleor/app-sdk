ae8d4a605d04289cdf532e0359a0e0e331bc7a8c
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-02-14 08:37:04 +0100

    Expose verify-jwks and remove legacy domain usage instead of api url

--- src/urls.test.ts
+++ src/urls.test.ts
@@ -1,26 +1,13 @@
 import { describe, expect, test } from "vitest";
 
-import { getGraphQLUrl, getJwksUrl } from "./urls";
+import { getJwksUrlFromSaleorApiUrl } from "./urls";
 
 describe("urls.ts", () => {
-  describe("jwksUrl function", () => {
-    test.each([
-      ["localhost:8000", "http://localhost:8000/.well-known/jwks.json"],
-      [
-        "https://my-saleor.saleor.cloud",
-        "https://https://my-saleor.saleor.cloud/.well-known/jwks.json",
-      ],
-    ])("resolves %s to be %s", (input, expectedOutput) => {
-      expect(getJwksUrl(input)).toBe(expectedOutput);
-    });
-  });
-
-  describe("graphQLUrl function", () => {
-    test.each([
-      ["localhost:8000", "http://localhost:8000/graphql/"],
-      ["https://my-saleor.saleor.cloud", "https://https://my-saleor.saleor.cloud/graphql/"],
-    ])("resolves %s to be %s", (input, expectedOutput) => {
-      expect(getGraphQLUrl(input)).toBe(expectedOutput);
+  describe("getJwksUrlFromSaleorApiUrl function", () => {
+    test("Resolves valid url from saleor api url", () => {
+      expect(getJwksUrlFromSaleorApiUrl("https://my-saleor.saleor.cloud")).toBe(
+        "https://my-saleor.saleor.cloud/.well-known/jwks.json"
+      );
     });
   });
 });
