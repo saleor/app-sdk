a17232632c36233e165db0e2479da12d66fad5b5
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-03-07 08:30:38 +0100

    Fix webhook legacy fields

--- src/handlers/next/saleor-webhooks/saleor-async-webhook.ts
+++ src/handlers/next/saleor-webhooks/saleor-async-webhook.ts
@@ -1,3 +1,4 @@
+import { ASTNode } from "graphql/index";
 import { NextApiHandler } from "next";
 
 import { AsyncWebhookEventType } from "../../../types";
@@ -9,19 +10,37 @@ export class SaleorAsyncWebhook<TPayload = unknown> extends SaleorWebhook<TPaylo
   protected readonly eventType = "async" as const;
 
   constructor(
-    configuration: WebhookConfig<AsyncWebhookEventType> & {
+    /**
+     * Omit new required fields and make them optional. Validate in constructor.
+     * In 0.35.0 remove old fields
+     */
+    configuration: Omit<WebhookConfig<AsyncWebhookEventType>, "event" | "query"> & {
       /**
        * @deprecated - use `event` instead. Will be removed in 0.35.0
        */
       asyncEvent?: AsyncWebhookEventType;
+      event?: AsyncWebhookEventType;
+      query?: string | ASTNode;
     }
   ) {
+    if (!configuration.event && !configuration.asyncEvent) {
+      throw new Error("event or asyncEvent must be provided. asyncEvent is deprecated");
+    }
+
+    if (!configuration.query && !configuration.subscriptionQueryAst) {
+      throw new Error(
+        "query or subscriptionQueryAst must be provided. subscriptionQueryAst is deprecated"
+      );
+    }
+
     super({
       ...configuration,
-      event: configuration.event ?? configuration.asyncEvent,
+      event: configuration.event! ?? configuration.asyncEvent!,
+      query: configuration.query! ?? configuration.subscriptionQueryAst!,
     });
 
-    this.event = configuration.event ?? configuration.asyncEvent;
+    this.event = configuration.event! ?? configuration.asyncEvent!;
+    this.query = configuration.query! ?? configuration.subscriptionQueryAst!;
   }
 
   createHandler(handlerFn: NextWebhookApiHandler<TPayload>): NextApiHandler {
