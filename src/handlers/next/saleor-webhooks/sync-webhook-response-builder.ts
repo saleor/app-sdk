f5643fba6b7062f1c2efe984be47a20359cddaa2
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-03-07 11:02:37 +0100

    Update expected payloads

--- src/handlers/next/saleor-webhooks/sync-webhook-response-builder.ts
+++ src/handlers/next/saleor-webhooks/sync-webhook-response-builder.ts
@@ -7,24 +7,24 @@ type SyncWebhookResponsesMap = {
   CHECKOUT_CALCULATE_TAXES: {
     shipping_price_gross_amount: number;
     shipping_price_net_amount: number;
-    shipping_tax_rate: string;
+    shipping_tax_rate: number;
     lines: Array<{
       total_gross_amount: number;
       total_net_amount: number;
-      tax_rate: string;
+      tax_rate: number;
     }>;
   };
   CHECKOUT_FILTER_SHIPPING_METHODS: {
     excluded_methods: Array<{
       id: string;
-      reason: string;
+      reason?: string;
     }>;
   };
   ORDER_CALCULATE_TAXES: SyncWebhookResponsesMap["CHECKOUT_CALCULATE_TAXES"];
   ORDER_FILTER_SHIPPING_METHODS: SyncWebhookResponsesMap["CHECKOUT_FILTER_SHIPPING_METHODS"];
   SHIPPING_LIST_METHODS_FOR_CHECKOUT: Array<{
     id: string;
-    name: string;
+    name?: string;
     amount: number;
     currency: string; // or enum?
     maximum_delivery_days?: number;
