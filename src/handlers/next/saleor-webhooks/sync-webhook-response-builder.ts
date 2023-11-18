ee54e20cf4558abef85e0d67a43700e09f49ed36
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-11-18 10:44:31 +0100

    Add missing sync events payload definitions

--- src/handlers/next/saleor-webhooks/sync-webhook-response-builder.ts
+++ src/handlers/next/saleor-webhooks/sync-webhook-response-builder.ts
@@ -90,8 +90,126 @@ export type SyncWebhookResponsesMap = {
     externalUrl?: string;
     message?: string;
   };
-  
-} satisfies Record<SyncWebhookEventType, any>
+  PAYMENT_METHOD_PROCESS_TOKENIZATION_SESSION:
+    | {
+        result: "SUCCESSFULLY_TOKENIZED";
+        id: string;
+        data: unknown;
+      }
+    | {
+        result: "ADDITIONAL_ACTION_REQUIRED";
+        id: string;
+        data: unknown;
+      }
+    | {
+        result: "PENDING";
+        data: unknown;
+      }
+    | {
+        result: "FAILED_TO_TOKENIZE";
+        error: string;
+      };
+  PAYMENT_METHOD_INITIALIZE_TOKENIZATION_SESSION:
+    | {
+        result: "SUCCESSFULLY_TOKENIZED";
+        id: string;
+        data: unknown;
+      }
+    | {
+        result: "ADDITIONAL_ACTION_REQUIRED";
+        id: string;
+        data: unknown;
+      }
+    | {
+        result: "PENDING";
+        data: unknown;
+      }
+    | {
+        result: "FAILED_TO_TOKENIZE";
+        error: string;
+      };
+  PAYMENT_GATEWAY_INITIALIZE_TOKENIZATION_SESSION:
+    | {
+        result: "SUCCESSFULLY_INITIALIZED";
+        data: unknown;
+      }
+    | {
+        result: "FAILED_TO_INITIALIZE";
+        error: string;
+      };
+  STORED_PAYMENT_METHOD_DELETE_REQUESTED:
+    | {
+        result: "SUCCESSFULLY_DELETED";
+      }
+    | {
+        result: "FAILED_TO_DELETE";
+        error: string;
+      };
+  LIST_STORED_PAYMENT_METHODS: {
+    paymentMethods: Array<{
+      id: string;
+      supportedPaymentFlows: Array<"INTERACTIVE">; // todo confirm
+      type: string;
+      // todo confirm which are optional, also numbers vs strings
+      creditCardInfo?: {
+        brand: string;
+        lastDigits: string;
+        expMonth: string;
+        expYear: string;
+        firstDigits?: string;
+      };
+      name?: string;
+      data?: unknown;
+    }>;
+  };
+  // todo
+  PAYMENT_VOID: {};
+  // todo
+  PAYMENT_REFUND: {};
+  PAYMENT_PROCESS:
+    | {
+        action_required: true;
+        // todo verify
+        action_required_data: {
+          confirmation_url: "https://www.example.com/3ds-confirmation/";
+        };
+        customer_id: string;
+        // todo - confirm if all required
+        payment_method: {
+          brand: string;
+          exp_month: string;
+          exp_year: string;
+          last_4: string;
+          name: string;
+          type: string; // enum?
+        };
+        transaction_id: string;
+      }
+    | {
+        action_required: false;
+        kind: "auth"; // todo enum?
+        customer_id: string;
+        // todo - check if required all fields
+        payment_method: {
+          brand: string;
+          exp_month: string;
+          exp_year: string;
+          last_4: string;
+          name: string;
+          type: string; // enum?
+        };
+        transaction_id: string;
+      };
+  PAYMENT_LIST_GATEWAYS: Array<{
+    id: string;
+    name: string;
+    currencies: string[];
+    config: unknown;
+  }>;
+  PAYMENT_CONFIRM: unknown; // todo
+  PAYMENT_CAPTURE: unknown; // todo
+  PAYMENT_AUTHORIZE: unknown; // todo
+};
 
 /**
  * Identity function, but it works on Typescript level to pick right payload based on first param
