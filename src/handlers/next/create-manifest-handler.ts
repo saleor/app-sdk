d24c734215cab5c062cab664aa1392b98ecc624e
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-05-20 12:48:28 +0200

    extra logs for manifest handler (#361)

--- src/handlers/next/create-manifest-handler.ts
+++ src/handlers/next/create-manifest-handler.ts
@@ -1,5 +1,6 @@
 import { NextApiHandler, NextApiRequest } from "next";
 
+import { createDebug } from "../../debug";
 import { getBaseUrl, getSaleorHeaders } from "../../headers";
 import { AppManifest } from "../../types";
 
@@ -12,6 +13,8 @@ export type CreateManifestHandlerOptions = {
   }): AppManifest | Promise<AppManifest>;
 };
 
+const debug = createDebug("create-manifest-handler");
+
 /**
  * Creates API handler for Next.js. Helps with Manifest creation, hides
  * implementation details if possible
@@ -23,11 +26,23 @@ export const createManifestHandler =
     const { schemaVersion } = getSaleorHeaders(request.headers);
     const baseURL = getBaseUrl(request.headers);
 
-    const manifest = await options.manifestFactory({
-      appBaseUrl: baseURL,
-      request,
-      schemaVersion,
-    });
+    debug("Received request with schema version \"%s\" and base URL \"%s\"", schemaVersion, baseURL);
 
-    return response.status(200).json(manifest);
+    try {
+      const manifest = await options.manifestFactory({
+        appBaseUrl: baseURL,
+        request,
+        schemaVersion,
+      });
+
+      debug("Executed manifest file");
+
+      return response.status(200).json(manifest);
+    } catch (e) {
+      debug("Error while resolving manifest: %O", e);
+
+      return response.status(500).json({
+        message: "Error resolving manifest file",
+      });
+    }
   };
