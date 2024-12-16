eaaae1a9df3d9b4ecb27839fc961972afc2bdbb1
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-12-16 16:02:42 +0100

    vercel kv exported as non-experimental

---
+++ .changeset/fuzzy-pans-develop.md
@@ -0,0 +1,5 @@
+---
+"@saleor/app-sdk": minor
+---
+
+Added VercelKV APL. TODO link to docs and example
--- src/APL/vercel-kv/index.ts
+++ src/APL/vercel-kv/index.ts
@@ -1,2 +1,3 @@
 // eslint-disable-next-line camelcase
 export { VercelKvApl as _experimental_VercelKvApl } from "./vercel-kv-apl";
+export { VercelKvApl } from "./vercel-kv-apl";
