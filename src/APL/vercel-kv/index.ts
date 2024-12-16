886a3fe0461ef506851a79550bf3296d52b559df
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-12-16 16:10:04 +0100

    deprecate old symbol

--- src/APL/vercel-kv/index.ts
+++ src/APL/vercel-kv/index.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated - use VercelKvApl
+ */
 // eslint-disable-next-line camelcase
 export { VercelKvApl as _experimental_VercelKvApl } from "./vercel-kv-apl";
 export { VercelKvApl } from "./vercel-kv-apl";
