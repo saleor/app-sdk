c4eb069d8d3a822f740dff4cc3ff96343443487b
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-01-02 15:33:25 +0100

    Fix broken build

--- src/handlers/fetch-api/index.ts
+++ src/handlers/fetch-api/index.ts
@@ -1,2 +1,7 @@
 export * from "./create-app-register-handler";
 export * from "./create-manifest-handler";
+export * from "./create-protected-handler";
+export * from "./process-protected-handler";
+// export * from "./saleor-webhooks/saleor-async-webhook";
+// export * from "./saleor-webhooks/saleor-sync-webhook";
+// export * from "./saleor-webhooks/sync-webhook-response-builder";
