d0bf8b190f50afed555e48f893242da0d02c7a6e
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-01-28 17:12:36 +0100

    Remove deprecated exports

--- src/handlers/platforms/next/index.ts
+++ src/handlers/platforms/next/index.ts
@@ -1,10 +1,6 @@
-// Re-export to avoid breaking changes
-export * from "../../shared/protected-action-validator";
-export * from "../../shared/sync-webhook-response-builder";
 export * from "./create-app-register-handler";
 export * from "./create-manifest-handler";
 export * from "./create-protected-handler";
-export * from "./process-protected-handler";
 export * from "./saleor-webhooks/saleor-async-webhook";
 export * from "./saleor-webhooks/saleor-sync-webhook";
 export { NextWebhookApiHandler } from "./saleor-webhooks/saleor-webhook";
