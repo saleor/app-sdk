07ace44ca4b5e9670cd590001949f8259b9755ac
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-01-29 17:13:43 +0100

    Add tests for adapter middleware

---
+++ src/handlers/shared/adapter-middleware.test.ts
@@ -0,0 +1,133 @@
+import { beforeEach, describe, expect, it } from "vitest";
+
+import {
+  SALEOR_API_URL_HEADER,
+  SALEOR_AUTHORIZATION_BEARER_HEADER,
+  SALEOR_EVENT_HEADER,
+  SALEOR_SCHEMA_VERSION,
+  SALEOR_SIGNATURE_HEADER,
+} from "@/const";
+import { MockAdapter } from "@/test-utils/mock-adapter";
+
+import { PlatformAdapterMiddleware } from "./adapter-middleware";
+
+describe("PlatformAdapterMiddleware", () => {
+  let mockAdapter: MockAdapter;
+
+  beforeEach(() => {
+    mockAdapter = new MockAdapter({});
+  });
+
+  describe("withMethod", () => {
+    it("returns null when method is allowed", () => {
+      mockAdapter.method = "POST";
+      const middleware = new PlatformAdapterMiddleware(mockAdapter);
+
+      const result = middleware.withMethod(["POST", "GET"]);
+
+      expect(result).toBeNull();
+    });
+
+    it("returns 405 error when method is not allowed", () => {
+      mockAdapter.method = "POST";
+      const middleware = new PlatformAdapterMiddleware(mockAdapter);
+
+      const result = middleware.withMethod(["GET"]);
+
+      expect(result).toEqual({
+        body: "Method not allowed",
+        bodyType: "string",
+        status: 405,
+      });
+    });
+  });
+
+  describe("withSaleorApiUrlPresent", () => {
+    it("returns null when saleor-api-url header is present", () => {
+      const adapter = new MockAdapter({
+        mockHeaders: {
+          [SALEOR_API_URL_HEADER]: "https://api.saleor.io"
+        }
+      })
+      const middleware = new PlatformAdapterMiddleware(adapter);
+
+      const result = middleware.withSaleorApiUrlPresent();
+
+      expect(result).toBeNull();
+    });
+
+    it("returns 400 error when saleor api url is missing", () => {
+      const middleware = new PlatformAdapterMiddleware(mockAdapter);
+
+      const result = middleware.withSaleorApiUrlPresent();
+
+      expect(result).toEqual({
+        body: "Missing saleor-api-url header",
+        bodyType: "string",
+        status: 400,
+      });
+    });
+  });
+
+  describe("getSaleorHeaders", () => {
+    it("correctly transforms header values", () => {
+      const adapter = new MockAdapter({
+        mockHeaders: {
+          [SALEOR_AUTHORIZATION_BEARER_HEADER]: "bearer-token",
+          [SALEOR_SIGNATURE_HEADER]: "signature-value",
+          [SALEOR_EVENT_HEADER]: "event-name",
+          [SALEOR_API_URL_HEADER]: "https://api.saleor.io",
+          [SALEOR_SCHEMA_VERSION]: "3.20",
+        }
+      })
+      const middleware = new PlatformAdapterMiddleware(adapter);
+
+      const result = middleware.getSaleorHeaders();
+
+      expect(result).toEqual({
+        authorizationBearer: "bearer-token",
+        signature: "signature-value",
+        event: "event-name",
+        saleorApiUrl: "https://api.saleor.io",
+        schemaVersion: 3.20,
+      });
+    });
+
+    it("handles missing values correctly - returns undefined", () => {
+      const middleware = new PlatformAdapterMiddleware(mockAdapter);
+
+      const result = middleware.getSaleorHeaders();
+
+      expect(result).toEqual({
+        authorizationBearer: undefined,
+        signature: undefined,
+        event: undefined,
+        saleorApiUrl: undefined,
+        schemaVersion: undefined,
+      });
+    });
+
+    it("handlers partially missing headers", () => {
+      const adapter = new MockAdapter({
+        mockHeaders: {
+          // SALEOR_AUTHORIZATION_BEARER_HEADER missing
+          [SALEOR_SIGNATURE_HEADER]: "signature-value",
+          [SALEOR_EVENT_HEADER]: "event-name",
+          [SALEOR_API_URL_HEADER]: "https://api.saleor.io",
+          // SALEOR_SCHEMA_VERSION missing
+        }
+      })
+      const middleware = new PlatformAdapterMiddleware(adapter);
+
+      const result = middleware.getSaleorHeaders();
+
+      expect(result).toEqual({
+        authorizationBearer: undefined,
+        signature: "signature-value",
+        event: "event-name",
+        saleorApiUrl: "https://api.saleor.io",
+        schemaVersion: undefined,
+      });
+    })
+  });
+});
