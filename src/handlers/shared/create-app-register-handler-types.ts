53bacde14efae06b290478d7433d058d4ce38e87
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-02-03 16:50:23 +0100

    Remove `saleorDomain` from type

--- src/handlers/shared/create-app-register-handler-types.ts
+++ src/handlers/shared/create-app-register-handler-types.ts
@@ -24,7 +24,6 @@ export type GenericCreateAppRegisterHandlerOptions<RequestType> = HasAPL & {
     request: RequestType,
     context: {
       authToken?: string;
-      saleorDomain?: string;
       saleorApiUrl?: string;
       respondWithError: CallbackErrorHandler;
     }
